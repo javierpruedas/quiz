8670634c19f577ef7afcfdedb30cf2f4de904f5a
Javier <javier.pruedasa@alumnos.upm.es>
2015-05-13 18:13:15 +0200

    PASO9

--- models/models.js
+++ models/models.js
@@ -29,12 +29,15 @@ exports.Quiz = Quiz; // exportar tabla Quiz
 // sequelize.sync() inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
 // then(..) ejecuta el manejador una vez creada la tabla
-Quiz.count().then(function (count){
-if(count === 0) { // la tabla se inicializa solo si está vacía
-Quiz.create({ pregunta: 'Capital de Italia',
-respuesta: 'Roma'
-})
-.then(function(){console.log('Base de datos inicializada')});
-};
-});
+Quiz.count().then(function(count){
+        if(count === 0){
+            Quiz.create({ pregunta: 'Capital de Italia',
+                          respuesta: 'Roma'
+                        });
+            Quiz.create({ pregunta: 'Capital de Portugal',
+                          respuesta: 'Lisboa'
+                        })
+            .then(function(){console.log('Base de datos inicializada')});
+        };
+    });
 });
